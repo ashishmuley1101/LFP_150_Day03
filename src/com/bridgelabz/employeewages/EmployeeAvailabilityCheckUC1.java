24b9e92c1b9f76c330d7012ce36cbaae965beef2
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-06-24 23:19:54 +0530

    EmployeeAvailabilityCheckUC1

---
+++ src/com/bridgelabz/employeewages/EmployeeAvailabilityCheckUC1.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.employeewages;
+
+public class EmployeeAvailabilityCheckUC1 {
+	public static void main(String[] args) {
+		int isPresent = 0;
+		final int PRESENT = 1;
+		System.out.println("Welcome to Employee Wage Computation Program...!!!");
+		System.out.println();
+		isPresent = (int)(Math.random()*2);
+		System.out.println(isPresent);
+		if(isPresent == PRESENT) {
+			System.out.println("Employee is present.");
+		}
+		else {
+			System.out.println("Employee is absent.");
+		}
+	}
+}
+
