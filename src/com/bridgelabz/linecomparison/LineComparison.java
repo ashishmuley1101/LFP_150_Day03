e5f7f88f824180a91f2634115a7e86f3e9b7d6b0
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-06-25 16:07:28 +0530

    LineComparison UC1

--- src/com/bridgelabz/linecomparison/LineComparison.java
+++ src/com/bridgelabz/linecomparison/LineComparison.java
@@ -1,5 +1,24 @@
 package com.bridgelabz.linecomparison;
 
+import java.util.Scanner;
+
 public class LineComparison {
+	public static void main(String[] args) {
+
+		String lengthofline = "0";
+		System.out.println("Welcome to Line Computation Program");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the x coordinate of first point of the line");
+		double x1 = sc.nextDouble();
+		System.out.println("Enter the y coordinate of first point of the line");
+		double y1 = sc.nextDouble();
+		System.out.println("Enter the x coordinate of second point of the line");
+		double x2 = sc.nextDouble();
+		System.out.println("Enter the y coordinate of second point of the line");
+		double y2 = sc.nextDouble();
+		lengthofline =String.valueOf(Math.floor((Math.sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1))))));
+		System.out.println("Length of the line is :" + lengthofline);
+		sc.close();
+	}
 
 }
